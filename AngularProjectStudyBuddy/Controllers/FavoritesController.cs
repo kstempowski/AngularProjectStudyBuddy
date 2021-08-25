9724641f2f33719908982eb98c48b42242d3dc47
anne <amlovell2@outlook.com>
2021-08-25 19:33:24 -0400

    8/25/21 anne

---
+++ AngularProjectStudyBuddy/Controllers/FavoritesController.cs
@@ -0,0 +1,37 @@
+﻿using AngularProjectStudyBuddy.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AngularProjectStudyBuddy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FavoritesController : ControllerBase
+    {
+        private readonly StudyBuddyDBContext _context;
+        public FavoritesController(StudyBuddyDBContext context)
+        {
+            _context = context;
+        }
+        //GET: api/favorite/{UserName}
+        [HttpGet("{UserName}")]
+        public async Task<ActionResult<Favorite>> GetFavorites(string UserName)
+        {
+            var favorites = await _context.Favorites.FindAsync(UserName);
+
+            if (favorites.UserName == UserName)
+            {
+                return favorites;
+            }
+            else
+            {
+                return NotFound();
+            }
+
+        }
+    }
+}
