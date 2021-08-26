d09555e2c62c1923a12d2b42bb0bc9fdb7b2b763
waelhawa <wael.hawa86@gmail.com>
2021-08-26 15:01:22 -0400

    082621 Fixed APIs

--- AngularProjectStudyBuddy/Startup.cs
+++ AngularProjectStudyBuddy/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
 
 namespace AngularProjectStudyBuddy
 {
@@ -24,6 +25,11 @@ namespace AngularProjectStudyBuddy
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<StudyBuddyDBContext>(options => options.UseSqlServer(connection));
+
+            services.AddControllers().AddNewtonsoftJson(options =>
+            {
+                options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+            });
 
             services.AddControllersWithViews();
             // In production, the Angular files will be served from this directory
